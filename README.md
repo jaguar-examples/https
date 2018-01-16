8655682c3760fad4d8b41144212706cca42ae80c
Ravi Teja Gudapati <tejainece@gmail.com>
2018-01-16 21:49:13 +0100

    README. Jaguar 1.2.x

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # https
 
-Simple jaguar application that show how to activate https.
+Example showing how to use HTTPs with Jaguar.
 
-Code `bin/server.dart`
+See code [`bin/server.dart`](https://github.com/jaguar-examples/https/blob/master/bin/server.dart)
 
-Certificates `bin/ssl/`
+Certificates are found at [`bin/ssl/`](https://github.com/jaguar-examples/https/tree/master/bin/ssl).
 
 ## Instructions
 
-Instructions can be found at https://jaguar-dart.github.io/getting_started.html
+Instructions can be found at [Jaguar Wiki](https://github.com/Jaguar-dart/jaguar/wiki/HTTPS).
+
+Learn how to generate SSL certificate using the following links:
+
+1. [Self signed](https://github.com/Jaguar-dart/jaguar/wiki/Self-signed-SSL-certificate)
+2. [Let's encrypt](https://github.com/Jaguar-dart/jaguar/wiki/Let's-encrypt)
--- bin/server.dart
+++ bin/server.dart
@@ -1,25 +1,17 @@
 import 'dart:io';
 import 'package:jaguar/jaguar.dart';
 
-import 'package:https/api.dart';
-
 main(List<String> args) async {
+  // Create SecurityContext from certificate and private key
+  final security = new SecurityContext()
+    ..useCertificateChain("bin/ssl/certificate.pem")
+    ..usePrivateKey("bin/ssl/keys.pem");
 
-  final ea = new JaguarExampleApi();
-
-  /// provide you ssl certificate to jaguar with password if needed
-  SecurityContext security = new SecurityContext()
-    ..useCertificateChain("bin/ssl/cert.pem")
-    ..usePrivateKey("bin/ssl/key.pem", password: "jaguarPass");
-
-  /// Configure Jaguar with you securityContext
-  Jaguar configuration = new Jaguar(multiThread: true, securityContext: security);
-
-  /// add your apis
-  configuration.addApi(ea);
-  // ..more apis
-
-  /// launch server
-  print("Serve on ${configuration.resourceName}");
-  await configuration.serve();
+  // Configure Jaguar with you securityContext
+  final server = new Jaguar(securityContext: security);
+  // Add your apis
+  server.get('/hello', (_) => 'Hello');
+  // Launch server
+  await server.serve();
+  print("Serving on ${server.resourceName} ...");
 }
